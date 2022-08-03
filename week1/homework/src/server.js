93b0ebd770c95f3beef557087bb957215a7682cf
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2022-08-03 10:32:43 +0200

    final commit tests passed

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,26 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        break;
+      case '/add':
+        state += 1;
+        break;
+      case '/subtract':
+        state -= 1;
+        break;
+      case '/reset':
+        state = 10;
+        break;
+      default:
+        const error = 'Not found';
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ error }));
+        return;
+    }
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ state }));
   });
 
   return server;
