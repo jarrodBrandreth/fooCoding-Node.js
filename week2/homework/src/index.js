7b67f23466299e2e4beacabe81a81874e0332b77
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2022-08-08 11:53:45 +0200

    edge case, index added to list log,

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -9,13 +9,6 @@ const [, , cmd, ...args] = process.argv;
 const dataFile = 'list.json';
 
 const printHelp = () => {
-  //   console.log(`Usage: node index.js [options]
-  // FooCoding Node.js Week 2 - Lecture To-Do App
-  // Options:
-  //   list          read all to-dos
-  //   write [to-do] add to-do
-  //   help          show this help text
-  //   `);
   fs.readFile('help.txt', 'utf8', (err, data) => {
     if (err) console.log(err);
     console.log(data);
@@ -26,9 +19,10 @@ const printList = () => {
   fs.readFile(dataFile, (err, data) => {
     if (err) throw err;
     const obj = JSON.parse(data);
-    console.log(`${obj.length} items in your to do list`);
-    obj.forEach((item) => {
-      console.log(item);
+    console.log(`${obj.length} items in your to do list `);
+    obj.forEach((item, index) => {
+      console.log(`${index}. ${item}`);
+      // console.log(`${index + 1}. ${item}`);
     });
   });
 };
@@ -40,7 +34,7 @@ const addItem = () => {
     args.forEach((item) => obj.push(item));
     fs.writeFile(dataFile, JSON.stringify(obj), (err) => {
       if (err) throw err;
-      console.log(args.join(' ') + ' added to your list');
+      console.log(args.join(', ') + ' added to your list');
     });
   });
 };
@@ -49,11 +43,16 @@ const removeItem = () => {
   fs.readFile(dataFile, (err, data) => {
     if (err) throw err;
     const obj = JSON.parse(data);
-    const removedItem = obj.splice(args, 1);
-    fs.writeFile(dataFile, JSON.stringify(obj), (err) => {
-      if (err) throw err;
-      console.log(`removed ${removedItem} from task list`);
-    });
+    const index = args[0];
+    if (index >= 0 && index < obj.length) {
+      const removedItem = obj.splice(index, 1);
+      fs.writeFile(dataFile, JSON.stringify(obj), (err) => {
+        if (err) throw err;
+        console.log(`removed ${removedItem} from task list`);
+      });
+    } else {
+      console.log('invalid index');
+    }
   });
 };
 
@@ -61,15 +60,15 @@ const updateItem = () => {
   fs.readFile(dataFile, (err, data) => {
     if (err) throw err;
     const obj = JSON.parse(data);
-    const indexToUpdate = obj.indexOf(args[0]);
-    if (indexToUpdate >= 0) {
-      const updatedItem = obj.splice(indexToUpdate, 1, args[1]);
+    const index = args[0];
+    if (index >= 0 && index < obj.length) {
+      const updatedItem = obj.splice(index, 1, args[1]);
       fs.writeFile(dataFile, JSON.stringify(obj), (err) => {
         if (err) throw err;
         console.log(`updated ${updatedItem} with ${args[1]}`);
       });
     } else {
-      console.log('that item is not in your list');
+      console.log('invalid list number');
     }
   });
 };
